523a8f75063333d61f38179d8d7a83d05bda5763
Austin Caudill <31804903+AustinCaudill@users.noreply.github.com>
2021-09-15 17:48:27 -0500

    Update Dallas-Crime-Predictions.py

--- Dallas-Crime-Predictions.py
+++ Dallas-Crime-Predictions.py
@@ -1,6 +1,6 @@
 """ 
 Dallas Crime Predictions
-Sept 14th 2021
+Sept 15th 2021
 Austin Caudill
 
 """
@@ -10,23 +10,111 @@ import time
 start_time = time.time()
 
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-print("Load Successful.")
+print("Imports Loaded Successfully.")
 # ===============================
 
-data = pd.read_csv("Police_Incidents.csv", parse_dates=['Date1 of Occurrence'])
+data = pd.read_csv("Police_Incidents.csv", parse_dates=['Date1 of Occurrence', 'Year1 of Occurrence'])
 # Columns 3, 41, 52, 57, 60, 74 have mixed data types. Examine.
 # print(data.iloc[:, [3, 3]])
 
+# Rename columns for ease of use.
 data.rename(columns={'Date1 of Occurrence': 'Date'}, inplace=True)
+data.rename(columns={'Year1 of Occurrence': 'Year'}, inplace=True)
+
+# Good records weren't kept until mid-2014, so only want to keep records for subsequent years.
+data = data[data.Year >= "2015"]
+
+# Check to ensure records were removed.
 print('First date: ', str(data.Date.describe(datetime_is_numeric=True)['min']))
 print('Last date: ', str(data.Date.describe(datetime_is_numeric=True)['max']))
+
+# Check for duplicate records.
+print('Number of duplicate records: %s'  % (data.duplicated().sum()))
+# There are no duplicates in this dataset.
+
+
+
+# Analyzing number of incidents per day
+col = sns.color_palette()
+
+plt.figure(figsize=(10, 6))
+dates = data.groupby('Date').count().iloc[:, 0]
+sns.kdeplot(data=dates, shade=True)
+plt.axvline(x=dates.median(), ymax=0.95, linestyle='--', color=col[2])
+plt.annotate(
+    'Median: ' + str(dates.median()),
+    xy=(dates.median(), 0.004),
+    xytext=(200, 0.005),
+    arrowprops=dict(arrowstyle='->', color=col[1], shrinkB=10))
+plt.title(
+    'Distribution of Number of Incidents per Day', fontdict={'fontsize': 16})
+plt.xlabel('Incidents')
+plt.ylabel('Density')
+plt.show()
+
+# Analyzing number of incidents per year
+plt.figure(figsize=(10, 6))
+years = data.groupby('Year').count().iloc[:, 0]
+sns.kdeplot(data=years, shade=True)
+plt.axvline(x=years.median(), ymax=2, linestyle='--', color=col[2])
+plt.annotate(
+    'Median: ' + str(years.median()),
+    xy=(years.median(), 0.004),
+    xytext=(200, 0.005),
+    arrowprops=dict(arrowstyle='->', color=col[1], shrinkB=10))
+plt.title('Distribution of Number of Incidents per Year', fontdict={'fontsize': 16})
+plt.xlabel('Incidents')
+plt.ylabel('Density')
+plt.show()
+
+
+byday = data.groupby('Day1 of the Week').count().iloc[:, 0]
+byday = byday.reindex([
+    'Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'])
+plt.figure(figsize=(10, 5))
+with sns.axes_style("whitegrid"):
+    ax = sns.barplot(
+        byday.index, (byday.values / byday.values.sum()) * 100,
+        orient='v',
+        palette=cm.ScalarMappable(cmap='Reds').to_rgba(byday.values))
+plt.title('Incidents per Weekday', fontdict={'fontsize': 16})
+plt.xlabel('Weekday')
+plt.ylabel('Incidents (%)')
+plt.show()
+
+
+byincident = data.groupby('NIBRS Crime Category').count().iloc[:, 0].sort_values(ascending=False).head(15)
+byincident = byincident.reindex(np.append(np.delete(byincident.index, 0), 'MISCELLANEOUS'))
+plt.figure(figsize=(10, 10))
+with sns.axes_style("whitegrid"):
+    ax = sns.barplot(
+        (byincident.values / byincident.values.sum()) * 100,
+        byincident.index,
+        orient='h',
+        palette="Reds_r")
+plt.title('Incidents per Crime Category', fontdict={'fontsize': 16})
+plt.xlabel('Incidents (%)')
+plt.show()
+
+
+
+
+
+
+
+
 
 # Split dataframe into train and test datasets.
 train, test = train_test_split(data, test_size=0.33, random_state=42)
 
+
+
+# ===============================
 print("Success")
 print("--- %s seconds ---" % (time.time() - start_time))
